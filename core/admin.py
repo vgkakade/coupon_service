d02aca0d369f4515957ba1156d1fc60c8e5c7b3c
Vaibhav <vaibhav.kakadeg@gmail.com>
2025-07-11 08:40:35 +0530

    Fixed admin and updated migrations to include correct values

--- core/admin.py
+++ core/admin.py
@@ -1,21 +1,34 @@
 from django.contrib import admin
 from .models import CouponType, Coupon, CouponRedemption
 
+
 @admin.register(Coupon)
-class CouponTypeAdmin(admin.ModelAdmin):
-    list_display = ('code', 'coupon_type', 'discount_value', 'is_active', 'min_cart_value', 'created_at', 'updated_at', 'usage_limit', 'valid_from', 'valid_to')
-    search_fields = ('code', 'coupon_type__name')
-    list_filter = ('is_active', 'coupon_type')
+class CouponAdmin(admin.ModelAdmin):
+    list_display = (
+        "code",
+        "coupon_type",
+        "discount_value",
+        "is_active",
+        "min_cart_value",
+        "created_at",
+        "updated_at",
+        "usage_limit",
+        "valid_from",
+        "valid_to",
+    )
+    search_fields = ("code", "coupon_type__name")
+    list_filter = ("is_active", "coupon_type")
 
 
 @admin.register(CouponType)
 class CouponTypeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created_at', 'updated_at')
-    search_fields = ('name',)
+    list_display = ("code", "name", "created_at", "updated_at")
+    search_fields = ("name",)
+
 
 @admin.register(CouponRedemption)
 class CouponRedemptionAdmin(admin.ModelAdmin):
-    list_display = ('coupon', 'user_id', 'redeemed_at', 'order_id', 'is_active')
-    search_fields = ('coupon__code', 'user_id', 'order_id')
-    list_filter = ('is_active',)
-    raw_id_fields = ('coupon',)
+    list_display = ("coupon", "user_id", "redeemed_at", "order_id", "is_active")
+    search_fields = ("coupon__code", "user_id", "order_id")
+    list_filter = ("is_active",)
+    raw_id_fields = ("coupon",)
