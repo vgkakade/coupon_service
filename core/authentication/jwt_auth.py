b8e20cbec14d11e5689daf57fcaa299e32dd1ff3
Vaibhav <vaibhav.kakadeg@gmail.com>
2025-07-12 17:08:20 +0530

    added custom authentication

---
+++ core/authentication/jwt_auth.py
@@ -0,0 +1,16 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from django.contrib.auth.models import AnonymousUser
+
+
+class SimpleUser:
+    def __init__(self, user_id):
+        self.id = user_id
+        self.is_authenticated = True
+
+
+class CustomJWTAuthentication(JWTAuthentication):
+    def get_user(self, validated_token):
+        user_id = validated_token.get("user_id")
+        if not user_id:
+            return AnonymousUser
+        return SimpleUser(user_id)
